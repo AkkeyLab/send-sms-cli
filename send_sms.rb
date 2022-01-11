a74d4994e463754ffbc675021227716eff72038e
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2022-01-11 13:51:54 +0900

    :sparkles: Create send sms process

--- send_sms.rb
+++ send_sms.rb
@@ -1,6 +1,15 @@
 require 'twilio-ruby'
 
-def send_sms(phone_number, body_text)
+ACCOUNT_SID = ''
+AUTH_TOKEN = ''
+FROM_NUMBER = ''
 
+def send_sms(phone_number, body_text)
+  client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
+  client.messages.create(
+    from: FROM_NUMBER,
+    to: phone_number,
+    body: body_text
+  )
 end
 
